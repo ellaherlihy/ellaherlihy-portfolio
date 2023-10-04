42db2ebbae8c9a494040ac65f7867ff0cf3ae4fd
Ella Herlihy <ellaherlihy@gmail.com>
2023-10-04 21:11:57 +0100

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { BiLogoReact } from "react-icons/bi";
 export default function Footer() {
   return (
     <div className="footer">
-      <div className="footer-text">Copyright Ella Herlihy 2023 · Built using React.js <BiLogoReact className="react-icon"/></div>
+      <div className="footer-text">Ella Herlihy 2023 · Built using React.js <BiLogoReact className="react-icon"/></div>
     </div>
   )
 }

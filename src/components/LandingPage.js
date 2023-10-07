8352a22baa9c52ded96369cc89fcea3dbc346fae
Ella Herlihy <ellaherlihy@gmail.com>
2023-10-07 19:20:40 +0100

    changed animation for title

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,21 +1,14 @@
 import React from "react";
-import { Fade } from "react-awesome-reveal";
-// import { SiRubyonrails } from "react-icons/si";
-// import { BiLogoReact } from "react-icons/bi";
-// import { AiOutlineHtml5 } from "react-icons/ai";
+// import { Fade } from "react-awesome-reveal";
+
 
 
 export default function LandingPage() {
   return (
 
       <div className="landing-page">
-        <h1><Fade duration={4000}>Ella Herlihy</Fade></h1>
-        <h2><Fade duration={4000}>Web Developer</Fade></h2>
-        {/* <div className="landing-page-icons">
-          <SiRubyonrails />
-          <BiLogoReact />
-          <AiOutlineHtml5 />
-        </div> */}
+        <h1 className="bounce-in-top">Ella Herlihy</h1>
+        <h2>Web Developer</h2>
         <div className="scallop"></div>
       </div>
   )

8f0c9aeb6b8a190465851a2305cb3d3e47fc3267
Ella Herlihy <ellaherlihy@gmail.com>
2024-01-08 14:16:33 +0000

    updated year in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { BiLogoReact } from "react-icons/bi";
 
 export default function Footer() {
   return (
     <div className="footer">
-      <div className="footer-text">Ella Herlihy 2023 · Built using React.js <BiLogoReact className="react-icon"/></div>
+      <div className="footer-text">Ella Herlihy · 2024</div>
     </div>
   )
 }
